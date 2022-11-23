1c89c267e66bc6afe25e1ebbf50a88ebea6a2e0c
jordanj83 <jordan.johnson@telus.com>
2022-11-23 12:55:04 -0800

    added name

--- client.js
+++ client.js
@@ -1,20 +1,36 @@
-const net = require("net");
+ const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
+    host: "165.227.47.243",
     port: 50541
-    
+  }); 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
   });
   conn.on("connect", () => {
-    console.log("hello!")
+    conn.write("Name: JBJ");
   });
+
+  conn.on("data", (data) => {
+    console.log(data)
+    
+  });
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
+  
+  
+
 };
 
+console.log("Connecting ...");
+connect();
 
 module.exports = {
   connect
-};
+}
+
+
--- play.js
+++ play.js
@@ -1,5 +1,12 @@
 const net = require("net");
 const {connect} = require("./client")
+
+
+console.log("Connecting ...");
+
+connect();
+
+
 // establishes a connection with the game server
 // const connect = function () {
 //   const conn = net.createConnection({
@@ -16,6 +23,67 @@ const {connect} = require("./client")
 //   return conn;
 // };
 
-console.log("Connecting ...");
 
-connect();
+
+
+//server code
+// const net = require('net');
+// const port = 8009;
+
+// const server = net.createServer();
+// const connectedClients = [];
+
+// const broadcast = (message, author) => {
+//   for (let cl of connectedClients) {
+//     cl.write(message);
+//   }
+// };
+
+// server.on('connection,' (clent) => {
+// connectedClients.push(client)
+//   console.log('connection has been made by client.')
+// client.setEncoding('utf8');
+
+// client.write('Welcome to my awesome server!')
+
+// client.on('data', (message) => {
+//   console.log(`Message recieved ${message}`);
+// if (message.startsWith('setName ')) {
+//   const clientName = message.replace(/setName /, '');
+//   console.log('setting client name to:', clientName);
+// client.name = clientName
+// } else {
+// broadcast(message, client)
+// }
+// })
+// })
+
+// server.listen(port, function (){
+//   console.log(`Server is listening on port=${port}`)
+// })
+
+// const net = require('net');
+// const port = 12350;
+
+// // 2.tcp.ngrok.io:12350
+
+// const connectionConfig = {
+//   port: port,
+//   host: '2.tcp.ngrok.io'
+// };
+
+// const client = net.createConnection(connectionConfig);
+
+// client.setEncoding('utf8');
+
+// client.on('connect', () => {
+//   console.log('client is connected to the server');
+// });
+
+// process.stdin.on('data', (message) => {
+//   client.write(message);
+// });
+
+// client.on('data', function(message){
+//   console.log('server sent:', message);
+// });
